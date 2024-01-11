62bd08f5d43d08488d52f4980a8bdaced2c03120
ChmyrRoma <roma822747@gmail.com>
2024-01-11 17:40:34 +0200

    - install mui-icons - add collapse to a site - create animation

--- src/components/component/business/SideBar/SideBar.tsx
+++ src/components/component/business/SideBar/SideBar.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Grid } from '@mui/material';
 import { NavLink } from 'react-router-dom';
+import * as classNames from 'classnames';
+
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import LastPageIcon from '@mui/icons-material/LastPage';
+import MenuIcon from '@mui/icons-material/Menu';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 import { useAppDispatch, useAppSelector } from '../../../../store/hooks';
 import { logOut } from '../../../../store/slices/user';
@@ -8,12 +14,13 @@ import { logOut } from '../../../../store/slices/user';
 import styles from './SideBar.module.scss';
 
 const routes = [
-  { title: 'Events Page', path: '/events' },
-  { title: 'Settings', path: '/settings' }
+  { title: 'Events Page', path: '/events', icon: <MenuIcon /> },
+  { title: 'Settings', path: '/settings', icon: <SettingsIcon /> }
 ]
 
 const SideBar = () => {
-  const { userInfo } = useAppSelector(state => state.user)
+  const [isCollapse, setIsCollapse] = useState(false);
+  const { userInfo } = useAppSelector(state => state.user);
 
   const dispatch = useAppDispatch();
 
@@ -21,34 +28,50 @@ const SideBar = () => {
     dispatch(logOut())
   }
 
+  const handleCollapse = () => setIsCollapse(!isCollapse);
+
+
   return (
-    <Box className={styles.sideBar}>
+    <Box className={classNames({ [styles.sideBar]: true, [styles.sideBar__collapse]: isCollapse })}>
       <Box className={styles.sideBar__content}>
         <Box className={styles.sideBar__pageBlock}>
+          <Box className={styles.sideBar__collapse_buttonBlock}>
+            {!isCollapse ? (
+              <FirstPageIcon fontSize="medium" className={styles.sideBar__collapse_button} onClick={handleCollapse} />
+            ) : (
+              <LastPageIcon fontSize="medium" className={styles.sideBar__collapse_button} onClick={handleCollapse} />
+            )}
+          </Box>
           {routes.map(el => (
             <Box key={el.title} className={styles.sideBar__component}>
               <NavLink
                 to={el.path}
                 className={({ isActive }) => isActive ? `${styles.sideBar__component_active}` : "" }
               >
-                {el.title}
+                {!isCollapse ? el.title : el.icon}
               </NavLink>
             </Box>
           ))}
         </Box>
         <Box>
-          <Grid className={styles.sideBar__container}>
-            <Grid className={styles.sideBar__avatar} />
-            <Grid className={styles.sideBar__userInfo}>
-              <Grid className={styles.sideBar__userInfo_name}>{userInfo?.role}</Grid>
-              <Grid className={styles.sideBar__userInfo_email}>{userInfo?.email}</Grid>
-            </Grid>
-          </Grid>
+          {!isCollapse ? (
+            <Box className={styles.sideBar__container}>
+              <Grid className={styles.sideBar__avatar} />
+              <Grid className={styles.sideBar__userInfo}>
+                <Grid className={styles.sideBar__userInfo_name}>{userInfo?.role}</Grid>
+                <Grid className={styles.sideBar__userInfo_email}>{userInfo?.email}</Grid>
+              </Grid>
+            </Box>
+          ) : (
+            <Box className={styles.sideBar__collapse_userInfo}>
+              <Grid className={styles.sideBar__avatar} />
+            </Box>
+          )}
         </Box>
       </Box>
 
       <Box className={styles.sideBar__footer}>
-        <Box className={styles.sideBar__footer_buttonContainer}>
+        <Box className={styles.sideBar__footer_button}>
           <button onClick={handleLogOut}>Logout</button>
         </Box>
       </Box>
